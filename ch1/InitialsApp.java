db6fe1a9f0cf36fa51a687b52acfc84a5675d35b
aris charalampides <aris_charalampides@hotmail.com>
2025-01-04 22:22:12 +0200

    chapter-1 v0.1

---
+++ ch1/InitialsApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch1;
+/** Prints first letters of my name*/
+
+public class InitialsApp {
+    public static void main(String[] args) {
+        System.out.println("*         *          * ");
+        System.out.println(" *       *          *  * ");
+        System.out.println("  *     *          *    * ");
+        System.out.println("   *   *          *      * ");
+        System.out.println("    * *          *        * ");
+        System.out.println("     *          * * * * ** *  ");
+        System.out.println("    * *        *            * ");
+        System.out.println("   *   *      *              * ");
+        System.out.println("  *     *    *                * ");
+        System.out.println(" *       *  *                  * ");
+
+    }
+}
